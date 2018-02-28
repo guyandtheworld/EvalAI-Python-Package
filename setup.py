e1f5e60a983ff240a73f1dc165b9394c243ea273
Adarsh S <4darshofficial@gmail.com>
2018-02-28 19:29:29 +0530

    Setup and basic file structure defined

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+PROJECT = 'evalai'
+
+# Change docs/sphinx/conf.py too!
+VERSION = '0.1'
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='EvalAI',
+    long_description=long_description,
+
+    author='Adarsh S',
+    author_email='4darshofficial@gmail.com',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.2',
+                 'Intended Audience :: Developers',
+                 'Environment :: Console',
+                 ],
+
+    platforms=['Any'],
+
+    scripts=[],
+
+    provides=[],
+    install_requires=['cliff'],
+
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'evalai = evalai.cli.main:main'
+        ],
+        'evalai': [
+            'login = evalai.cli.auth:LoginCLI',
+        ],
+    },
+
+    zip_safe=False,
+)
