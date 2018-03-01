4ac04343cabc6c80038c31a4f92f5f53ba0ee451
Adarsh S <4darshofficial@gmail.com>
2018-03-01 23:46:33 +0530

    Arguments to Login

--- evalai/cli/auth.py
+++ evalai/cli/auth.py
@@ -8,7 +8,14 @@ class LoginCLI(Command):
 
     log = logging.getLogger(__name__)
 
-    def take_action(self, parsed_args):
+    def get_parser(self, prog_name):
+        parser = super(LoginCLI, self).get_parser(prog_name)
+        parser.add_argument('-u', '--username', help='username')
+        parser.add_argument('-p', '--password', help='password')
+        return parser
+
+    def take_action(self, parsed_args):        
         self.log.info('sending greeting')
+        self.log.info(parsed_args)
         self.log.debug('debugging')
         self.app.stdout.write('hi!\n')
