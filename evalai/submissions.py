3bdc9241320437d4c2f0833f5cebeaa6242fb01a
Adarsh S <4darshofficial@gmail.com>
2018-03-06 23:44:14 +0530

    Add more use cases

--- evalai/submissions.py
+++ evalai/submissions.py
@@ -1,20 +1,26 @@
 import base64
 import json
+import os
 import requests
 
 from utils import get_token
 
 
-def submit(challenge_title=None, challenge_phase_title=None, submission_file=None, domain="http://localhost:8000"):
+def submit(challenge=None, challenge_phase=None, submission_file=None, domain="http://localhost:8000"):
+    """
+    This is used to connect to submit a file to a specic phase of an active challenge
+    """
+
     CHALLENGE_ID = ""
     CHALLENGE_PHASE_ID = ""
+    PARTICIPATED = False
     TOKEN = get_token()
     AUTH_DETAILS = {
             "Authorization": "Token: {}".format(TOKEN),
     }
 
-    # Checks if the challenge exist and get the challenge id.
 
+    # Checks if the challenge exist and get the challenge id.
     challenges_url = "{}/api/challenges/challenge/present"
     challenges_url = challenges_url.format(domain)
     response = requests.get(challenges_url, AUTH_DETAILS)
@@ -27,12 +33,33 @@ def submit(challenge_title=None, challenge_phase_title=None, submission_file=Non
     if len(data['results']) == 0:
         raise ValueError('No challenges exists.')
 
-    for challenge in data['results']:
-        if challenge_title == challenge['title']:
-            CHALLENGE_ID = challenge['id']
+    for json_challenge in data['results']:
+        if challenge == json_challenge['title']:
+            CHALLENGE_ID = json_challenge['id']
+            break
 
     if CHALLENGE_ID == "":
         raise ValueError('Challenge with that name doesn\'t exist.')
+
+
+    # Checks if the user is a participant of the Challenge.
+
+    participant_url = "{}/api/participant_teams/challenges/{}/user"
+    participant_url = participant_url.format(domain, CHALLENGE_ID)
+
+    response = requests.get(challenges_url, AUTH_DETAILS)
+
+    data = json.loads(response.text)
+    print data
+    if len(data['results']) == 0:
+        raise ValueError('You haven\'t participated in any challenges.')
+
+    for json_challenge in data['results']:
+        if challenge == json_challenge['title']:
+            PARTICIPATED = True
+
+    if not PARTICIPATED:
+        raise ValueError('You have to participate to submit files!')
 
     # Checks if the challenge phase exist and get the challenge phase id.
 
@@ -47,25 +74,27 @@ def submit(challenge_title=None, challenge_phase_title=None, submission_file=Non
     if len(data['results']) == 0:
         raise ValueError('No challenges phases exists.')
 
-    for challenge_phase in data['results']:
-        if challenge_phase_title == challenge_phase['name']:
-            CHALLENGE_PHASE_ID = challenge_phase['id']
+    for json_challenge_phase in data['results']:
+        if challenge_phase == json_challenge_phase['name']:
+            CHALLENGE_PHASE_ID = json_challenge_phase['id']
 
     if CHALLENGE_PHASE_ID == "":
         raise ValueError('Challenge phase with that name doesn\'t exist.')
 
-    print CHALLENGE_ID, CHALLENGE_PHASE_ID
-
-    # Checking if the user is a participant of the challenge.
 
     # Doing the submissions.
 
     submission_url = "{}/api/jobs/challenge/{}/challenge_phase/{}/submission/"
     submission_url = submission_url.format(domain, CHALLENGE_ID, CHALLENGE_PHASE_ID)
-    files = {'file': open('results.txt', 'rb')}
+
+    # Location of the submission file
+    __location__ = os.path.realpath(os.path.join(os.getcwd(),
+                                os.path.dirname(__file__)))
+
+    file = {'file': open(os.path.join(__location__, submission_file), 'rb')}
+
     AUTH_DETAILS = {
             "Authorization": "Token: {}".format(TOKEN),
     }
 
-    response = requests.get(submission_url, AUTH_DETAILS)
-    print response.text
+    response = requests.post(submission_url, AUTH_DETAILS)
--- evalai/utils.py
+++ evalai/utils.py
@@ -1,11 +1,17 @@
 import base64
+import os
 import pickle
 
 
 def get_token():
+    """
+    Loads the user's token after logging in.
+    """
+    __location__ = os.path.realpath(os.path.join(os.getcwd(),
+                                    os.path.dirname(__file__)))
     outputFile = '.data'
     data = []
-    with open(outputFile, 'rb') as fr:
+    with open(os.path.join(__location__, outputFile), 'rb') as fr:
         data = pickle.load(fr)
     token = base64.b64decode(data["Token"])
     return token
