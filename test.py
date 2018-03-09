ea907174c9e3f59d1c730a96236a5a417e7dfcd9
Adarsh S <4darshofficial@gmail.com>
2018-03-09 21:11:57 +0530

    Change directory structure

--- test.py
+++
@@ -1,5 +0,0 @@
-from evalai import auth, submissions
-
-auth.login("mandan3", "mandan123")
-# submissions.leaderboard("kundi", "Jeffrey Phase")
-submissions.show_all_my_submissions("kundi", "Jeffrey Phase")
