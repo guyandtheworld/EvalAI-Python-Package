c5ad3ef3658dab820ef0de16b0222268ddb9c5f0
Adarsh S <4darshofficial@gmail.com>
2018-03-17 19:33:09 +0530

    Add testing docs

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
 from evalai import auth, submissions
 
-auth.login("user1", "password123")
+
+details = {
+            'method_name': "CNN_01",
+            'method_description': "First Iteration",
+            'project_url': "N/A",
+            'publication_url': "N/A",
+          }
+
+submissions.submit(
+                   challenge="Oasis",
+                   challenge_phase="Jeffrey Phase",
+                   submission_file="results.csv",
+                   submissions_details=details
+                   )
+
+
+
+
+
+# auth.login("user1", "passwor123")
+# submissions.leaderboard("Oasis", "Jeffrey Phase")
+# submissions.show_all_my_submissions("Oasis", "Jeffrey Phase")
+submissions.show_my_last_submission("Oasis", "Jeffrey Phase")
